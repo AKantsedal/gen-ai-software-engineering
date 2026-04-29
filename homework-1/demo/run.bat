a9c45072b361364a58c0efefa2e9724bd996cb7d
a.kantsedal <a.kantsedal@upstars.com>
2026-04-29 18:47:12 +0200

    fix bat file

--- homework-1/demo/run.bat
+++ homework-1/demo/run.bat
@@ -1,15 +1,13 @@
 @echo off
 cd /d "%~dp0..\src\BankingApi"
-
 echo Building BankingApi...
-dotnet build --nologo -q
+dotnet build --nologo
 if errorlevel 1 (
     echo Build failed.
     pause
     exit /b 1
 )
-
 echo Starting BankingApi on http://localhost:3000
 echo Swagger UI: http://localhost:3000/swagger
 echo Press Ctrl+C to stop.
-dotnet run --no-build
+dotnet run --no-build --project . --urls http://localhost:3000
